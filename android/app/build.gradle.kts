87418790f67bc5191526f505e4394b3a2d762b60
Nikhil Bhagat <knewboy.nykhil@gmail.com>
2026-05-24 13:11:54 +0545

    chore(android): release 2.0.0-alpha.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "app.paperkeep"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "2.0.0-alpha.1"
+        versionCode = 2
+        versionName = "2.0.0-alpha.2"
 
         testInstrumentationRunner = "app.paperkeep.HiltTestRunner"
     }
